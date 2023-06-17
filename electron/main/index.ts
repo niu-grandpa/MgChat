e942167177172070909ad16a63567d2cc3a6f58e
niu-grandpa <2864103063@qq.com>
2023-06-17 17:45:59 +0800

    优化代码

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,9 +1,7 @@
-import { BrowserWindow, app, shell } from 'electron';
+import { BrowserWindow, app } from 'electron';
 import { release } from 'node:os';
 import { join } from 'node:path';
-import pkg from '../../package.json';
-import winHandler, { loadFile } from './handler';
-import { update } from './update';
+import client from './client';
 
 // The built directory structure
 //
@@ -38,66 +36,25 @@ if (!app.requestSingleInstanceLock()) {
 // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 
 let win: BrowserWindow | null = null;
-// 缓存所有已创建过的窗口，key为pathname或pathname+search
-const winMap = new Map<string, BrowserWindow>();
-// Here, you can also use other preload
-const preload = join(__dirname, '../preload/index.js');
-const indexHtml = join(process.env.DIST, 'index.html');
-const [width, height] = pkg.debug.winSize;
 
-async function createMainWindow() {
-  win = new BrowserWindow({
-    width,
-    height,
-    title: 'Main window',
-    icon: join(process.env.PUBLIC, 'favicon.ico'),
-    frame: false,
-    resizable: false,
-    alwaysOnTop: true,
-    titleBarStyle: 'hidden',
-    webPreferences: {
-      preload,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-
-  loadFile({ win, key: '', indexHtml });
-
-  // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString());
-  });
-
-  // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
-    if (url.startsWith('https:')) shell.openExternal(url);
-    return { action: 'deny' };
-  });
-
-  winMap.set('main', win);
-
-  // Apply electron-updater
-  update(win);
-}
+const createMainWindow = () => {
+  client.capacity = 3;
+  client.registerListeners();
+  win = client.createMain('/');
+};
 
 app.whenReady().then(createMainWindow);
 
 app.on('window-all-closed', () => {
   win = null;
-  winMap.clear();
+  client.destroy();
   if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('second-instance', () => {
-  if (win) {
-    // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore();
-    win.focus();
-  }
+  // Focus on the main window if the user tried to open another
+  if (win?.isMinimized()) win?.restore();
+  win?.focus();
 });
 
 app.on('activate', () => {
@@ -107,11 +64,4 @@ app.on('activate', () => {
   } else {
     createMainWindow();
   }
-});
-
-winHandler({
-  win,
-  preload,
-  indexHtml,
-  map: winMap,
 });
